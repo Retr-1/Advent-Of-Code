c6ee2f68899a1218cb7a1d0bdb87a9570c6e1861
Chuck Norris <me@example.com>
2023-12-20 15:30:22 +0100

    Implemented the idea

--- 2023/18/a.py
+++ 2023/18/a.py
@@ -1,5 +1,23 @@
-lines = list(map(lambda x: x.strip(), open('input', 'r').readlines()))
-edging = set()
+def getInfo(x1, y1, x2, y2):
+   return x1*y2 - y1*x2
+
+def solve(points):
+   N = len(points)
+   firstx, firsty = points[0]
+   prevx, prevy = firstx, firsty
+   res = 0
+
+   for i in range(1, N):
+      nextx, nexty = points[i]
+      res = res + getInfo(prevx,prevy,nextx,nexty)
+      prevx = nextx
+      prevy = nexty
+   res = res + getInfo(prevx,prevy,firstx,firsty)
+   return abs(res)/2.0
+
+
+lines = list(map(lambda x: x.strip(), open('test', 'r').readlines()))
+edging = []
 x = y = 0
 DIRECTIONS = {
     'L': [-1, 0],
@@ -13,7 +31,6 @@ for i in range(len(lines)):
     for j in range(int(count)):
         x += dx
         y += dy
-        edging.add((x,y))
+        edging.append((x,y))
 
-topleft = min(edging, key=lambda x: (x[1],x[0]))
-bottomright = max()
+print(solve(edging), len(edging))
