d16f775e2e9f943b9c29d79801bf8bf99b549703
Chuck Norris <me@example.com>
2023-12-21 22:01:25 +0100

    Solved 20a, tried b

---
+++ 2023/20/a.py
@@ -0,0 +1,83 @@
+from collections import defaultdict
+
+LOW = False
+HIGH = True
+NO_RESPONSE = -1
+
+class FlipFlop:
+    def __init__(self) -> None:
+        self.state = LOW
+
+    def send(self, name, signal):
+        if signal == LOW:
+            self.state = not self.state
+            return self.state
+        return NO_RESPONSE
+    
+class Conjunction:
+    def __init__(self, ingoing=None) -> None:
+        self.ingoing = {}#defaultdict(bool)
+        #self.ingoing = dict(zip(ingoing, [LOW]*len(ingoing)))
+
+    def send(self, sender, signal):
+        assert sender in self.ingoing
+        self.ingoing[sender] = signal
+        if all(self.ingoing.values()):
+            return LOW
+        return HIGH
+
+class Broadcaster:
+    def send(self, name, signal):
+        return signal
+    
+
+def push_the_button():
+    batch = [('broadcaster', LOW, 'button')]
+    while len(batch) > 0:
+        name, signal, sender = batch.pop(0)
+        SS[signal] += 1
+        if not name in modules:
+            continue
+        obj, dest = modules[name]
+        response = obj.send(sender, signal)
+        if response != NO_RESPONSE:
+            for x in dest:
+                batch.append((x, response, name))
+
+
+
+            
+modules = {}
+SS = [0,0] # Signals-Sent
+lines = list(map(lambda x: x.strip(), open('input', 'r').readlines()))
+for line in lines:
+    for x in ['%', '&', 'broadcaster']:
+        if not line.startswith(x):
+            continue
+
+        a, b = line.split('->')
+        name = a[len(x):].strip()
+        dest = b.strip().split(', ')
+        if x == '%':
+            thing = FlipFlop()
+        elif x == '&':
+            thing = Conjunction()
+        else:
+            thing = Broadcaster()
+            name = x
+        modules[name] = [thing, dest]
+
+for x in modules:
+    o = modules[x][0]
+    if type(o) != Conjunction:
+        continue
+
+    for y in modules:
+        if x in modules[y][1]:
+            o.ingoing[y] = False
+
+
+for i in range(1000):
+    push_the_button()
+
+print(SS[0]*SS[1], SS)
