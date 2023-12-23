a2b8cfc383a7bcc76bf27176eefb7fba6794fd32
Chuck Norris <me@example.com>
2023-12-23 18:08:42 +0100

    Init

--- 2023/23/b.py
+++ 2023/23/b.py
@@ -1,31 +1,14 @@
 from sys import setrecursionlimit
 setrecursionlimit(10**8)
 
-
-def main(x, y, visited, dead_end):
-    if (x,y) == end:
-        return 1
-    
-    if dead_end[y][x]:
-        return float('-inf')
-    
-    visited[y][x] = True
-    best = float('-inf')
-    for nx, ny in [(x+1, y), (x-1,y), (x,y+1), (x,y-1)]:
-        if not (W > nx >= 0 and H > ny >= 0) or visited[ny][nx] or lines[ny][nx] == '#':
+def simplify(x, y):
+    for nx, ny, dr in [(x+1, y), (x-1,y), (x,y+1), (x,y-1)]:
+        if not (W > nx >= 0 and H > ny >= 0) or lines[ny][nx] == '#':
             continue
 
-        best = max(main(nx, ny, visited, dead_end), best)
-    
-    visited[y][x] = False
-    if best == float('-inf'):
-        dead_end[y][x] = True
-        
-    return best + 1
-        
+
 
 lines = list(map(lambda x: x.strip(), open('test', 'r').readlines()))
 W,H = len(lines[0]), len(lines)
 start = (1,0)
 end = (W-2, H-1)
-print(main(1, 0, [[False]*W for i in range(H)], [[False]*W for i in range(H)])-1)
